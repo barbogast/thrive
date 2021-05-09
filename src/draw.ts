30472da5dc38140e19b9d617d0ad887304d26f20
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-05-09 21:29:28 +0200

    Extract draw functions into draw.ts

---
+++ src/draw.ts
@@ -0,0 +1,119 @@
+import Konva from 'konva'
+
+import config from './config'
+import * as hexUtils from './hexUtils'
+import { Tile } from './types'
+
+function getCoordinates(row: number, col: number) {
+  const left = 250
+  const top = 250
+  const distance = 0
+  const r = config().tileRadius
+  if (config().flatTopped) {
+    const height = Math.sqrt(3) * r
+    const isOffset = row % 2 !== 0 ? (r * 2) / 2 : 0
+    return {
+      x: left + row * height,
+      y: top + col * (r * 2 + distance) + isOffset,
+    }
+  } else {
+    const height = Math.sqrt(3) * r
+    const isOffset = col % 2 !== 0 ? (r * 2) / 2 : 0
+    return {
+      x: left + row * (r * 2 + distance) + isOffset,
+      y: top + left + col * height,
+    }
+  }
+}
+
+export function drawTile(layer: Konva.Layer, info: Tile) {
+  const {
+    position: { row, col },
+    color,
+  } = info
+
+  const axial = hexUtils.offsetToAxial(info.position)
+  drawHexagon(
+    layer,
+    getCoordinates(row, col),
+    color,
+    `r: ${axial.q}\nc: ${axial.r}`,
+  )
+}
+
+function drawHexagon(
+  layer: Konva.Layer,
+  pos: hexUtils.PixelPosition,
+  color: string,
+  label: string,
+) {
+  var group = new Konva.Group({
+    x: pos.x,
+    y: pos.y,
+  })
+
+  group.add(
+    new Konva.RegularPolygon({
+      sides: 6,
+      rotation: config().flatTopped ? 30 : 0,
+      radius: config().tileRadius + 1,
+      fill: color,
+      stroke: 'black',
+      strokeWidth: 1,
+      id: 'asdf' + pos.x + pos.y,
+    }),
+  )
+  group.add(
+    new Konva.Text({
+      text: label,
+      fontSize: 10,
+      fontFamily: 'Arial',
+    }),
+  )
+
+  const rect = new Konva.Rect({
+    offsetX: config().tileRadius / 2,
+    offsetY: config().tileRadius - 2,
+    width: config().tileRadius,
+    height: 10,
+    rotate: 30,
+    // fill: '#00D2FF',
+    id: 'haha',
+    stroke: 'black',
+    strokeWidth: 0,
+  })
+
+  rect.on('mouseover', function (evt) {
+    var shape = evt.target
+    document.body.style.cursor = 'pointer'
+    shape.scaleX(1.2)
+    shape.scaleY(1.2)
+    shape.setAttr('fill', 'red')
+    layer.draw()
+  })
+
+  rect.on('mouseout', function (evt) {
+    var shape = evt.target
+    document.body.style.cursor = 'default'
+    shape.scaleX(1)
+    shape.scaleY(1)
+    layer.draw()
+  })
+
+  group.add(rect)
+
+  const rect2 = new Konva.Rect({
+    offsetX: config().tileRadius / 2,
+    offsetY: config().tileRadius + 6,
+    width: config().tileRadius,
+    height: 15,
+    rotate: 30,
+    fill: 'brown',
+    id: 'haha',
+    stroke: 'black',
+    strokeWidth: 1,
+  })
+  group.add(rect2)
+
+  layer.add(group)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Konva from 'konva'
-import config from './config'
+import * as draw from './draw'
 import * as hexUtils from './hexUtils'
 import * as tileMap from './tileMap'
 import { Tile } from './types'
@@ -18,8 +18,6 @@ var stage = new Konva.Stage({
 })
 
 var layer = new Konva.Layer()
-
-const TILE_RADIUS = 50
 
 function getColor() {
   const colors = [
@@ -131,7 +129,7 @@ function init() {
   console.log(roads)
 
   for (const tile of tiles) {
-    drawAtCoordinate(tile)
+    draw.drawTile(layer, tile)
   }
 
   layer.add(
@@ -158,114 +156,6 @@ function init() {
 }
 
 init()
-
-function getCoordinates(row: number, col: number) {
-  const left = 250
-  const top = 250
-  const distance = 0
-  const r = config().tileRadius
-  if (config().flatTopped) {
-    const height = Math.sqrt(3) * r
-    const isOffset = row % 2 !== 0 ? (r * 2) / 2 : 0
-    return {
-      x: left + row * height,
-      y: top + col * (r * 2 + distance) + isOffset,
-    }
-  } else {
-    const height = Math.sqrt(3) * r
-    const isOffset = col % 2 !== 0 ? (r * 2) / 2 : 0
-    return {
-      x: left + row * (r * 2 + distance) + isOffset,
-      y: top + left + col * height,
-    }
-  }
-}
-
-function drawAtCoordinate(info: Tile) {
-  const {
-    position: { row, col },
-    color,
-  } = info
-
-  const axial = hexUtils.offsetToAxial(info.position)
-  drawHexagon(getCoordinates(row, col), color, `r: ${axial.q}\nc: ${axial.r}`)
-}
-
-function drawHexagon(
-  pos: hexUtils.PixelPosition,
-  color: string,
-  label: string,
-) {
-  var group = new Konva.Group({
-    x: pos.x,
-    y: pos.y,
-  })
-
-  group.add(
-    new Konva.RegularPolygon({
-      sides: 6,
-      rotation: config().flatTopped ? 30 : 0,
-      radius: config().tileRadius + 1,
-      fill: color,
-      stroke: 'black',
-      strokeWidth: 1,
-      id: 'asdf' + pos.x + pos.y,
-    }),
-  )
-  group.add(
-    new Konva.Text({
-      text: label,
-      fontSize: 10,
-      fontFamily: 'Arial',
-    }),
-  )
-
-  const rect = new Konva.Rect({
-    offsetX: TILE_RADIUS / 2,
-    offsetY: TILE_RADIUS - 2,
-    width: TILE_RADIUS,
-    height: 10,
-    rotate: 30,
-    // fill: '#00D2FF',
-    id: 'haha',
-    stroke: 'black',
-    strokeWidth: 0,
-  })
-
-  rect.on('mouseover', function (evt) {
-    var shape = evt.target
-    document.body.style.cursor = 'pointer'
-    shape.scaleX(1.2)
-    shape.scaleY(1.2)
-    shape.setAttr('fill', 'red')
-    layer.draw()
-  })
-
-  rect.on('mouseout', function (evt) {
-    var shape = evt.target
-    document.body.style.cursor = 'default'
-    shape.scaleX(1)
-    shape.scaleY(1)
-    layer.draw()
-  })
-
-  group.add(rect)
-
-  const rect2 = new Konva.Rect({
-    offsetX: TILE_RADIUS / 2,
-    offsetY: TILE_RADIUS + 6,
-    width: TILE_RADIUS,
-    height: 15,
-    rotate: 30,
-    fill: 'brown',
-    id: 'haha',
-    stroke: 'black',
-    strokeWidth: 1,
-  })
-  group.add(rect2)
-
-  layer.add(group)
-}
 
 var scaleBy = 1.05
 stage.on('wheel', (e) => {
