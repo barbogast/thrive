fe3e5e6d64bb5c304aa0a3e18da0f6a41449acbf
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-05-13 18:02:12 +0200

    Move stage definition to Board.tsx

--- src/compontents/App.tsx
+++ src/compontents/App.tsx
@@ -1,45 +1,10 @@
-import { KonvaEventObject } from 'konva-types/Node'
-import { Stage as StateType } from 'konva-types/Stage'
 import React, { useEffect } from 'react'
-import { Stage, useStrictMode } from 'react-konva'
 import useConnection from '../useConnection'
 
 import useStore from '../state'
 import Board from './Board'
 import Controls from './Controls'
 import Players from './Players'
-
-useStrictMode(true)
-
-function onWheel(e: KonvaEventObject<WheelEvent>) {
-  const scaleBy = 1.05
-
-  const stage = e.currentTarget as StateType
-
-  e.evt.preventDefault()
-  var oldScale = stage.scaleX()
-
-  var pointer = stage.getPointerPosition()
-
-  if (!pointer) {
-    return
-  }
-  var mousePointTo = {
-    x: (pointer.x - stage.x()) / oldScale,
-    y: (pointer.y - stage.y()) / oldScale,
-  }
-
-  var newScale = e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy
-
-  stage.scale({ x: newScale, y: newScale })
-
-  var newPos = {
-    x: pointer.x - mousePointTo.x * newScale,
-    y: pointer.y - mousePointTo.y * newScale,
-  }
-  stage.position(newPos)
-  stage.batchDraw()
-}
 
 function App(): JSX.Element {
   const { initialise, state } = useStore((state) => ({
@@ -65,14 +30,7 @@ function App(): JSX.Element {
       <button onClick={connect}>Connect</button>
       <Players />
       <Controls />
-      <Stage
-        width={window.innerWidth}
-        height={window.innerHeight}
-        onWheel={onWheel}
-        draggable
-      >
-        <Board />
-      </Stage>
+      <Board />
       &nbsp;&nbsp;
       <button onClick={() => console.log(state)}>Log state</button>
       <button onClick={() => sendState()}>Send state</button>
--- src/compontents/Board.tsx
+++ src/compontents/Board.tsx
@@ -1,11 +1,45 @@
 import React from 'react'
-import { Layer } from 'react-konva'
+import { Layer, Stage, useStrictMode } from 'react-konva'
+import { KonvaEventObject } from 'konva-types/Node'
+import { Stage as StateType } from 'konva-types/Stage'
 
 import useStore from '../state'
 import HexTile from './HexTile'
 import Road from './Road'
 import Town from './Town'
 import { ActionType } from '../state'
+
+useStrictMode(true)
+
+function onWheel(e: KonvaEventObject<WheelEvent>) {
+  const scaleBy = 1.05
+
+  const stage = e.currentTarget as StateType
+
+  e.evt.preventDefault()
+  var oldScale = stage.scaleX()
+
+  var pointer = stage.getPointerPosition()
+
+  if (!pointer) {
+    return
+  }
+  var mousePointTo = {
+    x: (pointer.x - stage.x()) / oldScale,
+    y: (pointer.y - stage.y()) / oldScale,
+  }
+
+  var newScale = e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy
+
+  stage.scale({ x: newScale, y: newScale })
+
+  var newPos = {
+    x: pointer.x - mousePointTo.x * newScale,
+    y: pointer.y - mousePointTo.y * newScale,
+  }
+  stage.position(newPos)
+  stage.batchDraw()
+}
 
 function Board(): JSX.Element {
   const {
@@ -17,29 +51,36 @@ function Board(): JSX.Element {
   }))
 
   return (
-    <Layer>
-      {Object.values(tiles).map((t, i) => (
-        <HexTile key={i} tile={t} />
-      ))}
-
-      {currentAction.type === ActionType.buildRoad &&
-        Object.values(currentAction.positions).map((r, i) => (
-          <Road key={i} position={r} />
+    <Stage
+      width={window.innerWidth}
+      height={window.innerHeight}
+      onWheel={onWheel}
+      draggable
+    >
+      <Layer>
+        {Object.values(tiles).map((t, i) => (
+          <HexTile key={i} tile={t} />
         ))}
 
-      {currentAction.type === ActionType.buildTown &&
-        Object.values(currentAction.positions).map((r, i) => (
-          <Town key={i} position={r} />
+        {currentAction.type === ActionType.buildRoad &&
+          Object.values(currentAction.positions).map((r, i) => (
+            <Road key={i} position={r} />
+          ))}
+
+        {currentAction.type === ActionType.buildTown &&
+          Object.values(currentAction.positions).map((r, i) => (
+            <Town key={i} position={r} />
+          ))}
+
+        {Object.values(roads).map((r, i) => (
+          <Road key={i} position={r.position} owner={r.owner} />
         ))}
 
-      {Object.values(roads).map((r, i) => (
-        <Road key={i} position={r.position} owner={r.owner} />
-      ))}
-
-      {Object.values(towns).map((t, i) => (
-        <Town key={i} position={t.position} owner={t.owner} />
-      ))}
-    </Layer>
+        {Object.values(towns).map((t, i) => (
+          <Town key={i} position={t.position} owner={t.owner} />
+        ))}
+      </Layer>
+    </Stage>
   )
 }
 
