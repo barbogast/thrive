05dd9a20307471099a1f480b27d958f251e48fe7
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-05-14 09:30:35 +0200

    Persist state in localStorage

--- src/state.ts
+++ src/state.ts
@@ -1,4 +1,5 @@
 import create from 'zustand'
+import { persist } from 'zustand/middleware'
 import produce from 'immer'
 
 import * as game from './game'
@@ -41,84 +42,89 @@ type Setter = {
   updateGameState: (gameState: game.GameState) => void
 }
 
-const useStore = create<State & Setter>((set) => {
-  const iSet = (fn: (state: State) => void) => set(produce(fn))
-  return {
-    gameState: {
-      tiles: {},
-      roads: [],
-      towns: [],
-      players: game.initialisePlayers(),
-      currentPlayer: game.PlayerId.green,
-      currentDiceRoll: [],
-    },
-    uiState: {
-      currentAction: { type: ActionType.none },
-    },
+const useStore = create<State & Setter>(
+  persist(
+    (set) => {
+      const iSet = (fn: (state: State) => void) => set(produce(fn))
+      return {
+        gameState: {
+          tiles: {},
+          roads: [],
+          towns: [],
+          players: game.initialisePlayers(),
+          currentPlayer: game.PlayerId.green,
+          currentDiceRoll: [],
+        },
+        uiState: {
+          currentAction: { type: ActionType.none },
+        },
 
-    initialise: () => iSet(() => ({ gameState: game.initialiseGame() })),
+        initialise: () => iSet(() => ({ gameState: game.initialiseGame() })),
 
-    buildTown: (position: position.Position) =>
-      iSet((draft) => {
-        game.buildTown(draft.gameState, position)
-        draft.uiState.currentAction = { type: ActionType.none }
-      }),
+        buildTown: (position: position.Position) =>
+          iSet((draft) => {
+            game.buildTown(draft.gameState, position)
+            draft.uiState.currentAction = { type: ActionType.none }
+          }),
 
-    buildRoad: (position: position.Position) =>
-      iSet((draft) => {
-        game.buildRoad(draft.gameState, position)
-        draft.uiState.currentAction = { type: ActionType.none }
-      }),
+        buildRoad: (position: position.Position) =>
+          iSet((draft) => {
+            game.buildRoad(draft.gameState, position)
+            draft.uiState.currentAction = { type: ActionType.none }
+          }),
 
-    nextTurn: () =>
-      iSet((draft) => {
-        draft.uiState.currentAction = { type: ActionType.none }
-        game.rollDice(draft.gameState)
-        draft.gameState.currentPlayer = game.getNextPlayer(
-          draft.gameState.currentPlayer,
-        )
-      }),
+        nextTurn: () =>
+          iSet((draft) => {
+            draft.uiState.currentAction = { type: ActionType.none }
+            game.rollDice(draft.gameState)
+            draft.gameState.currentPlayer = game.getNextPlayer(
+              draft.gameState.currentPlayer,
+            )
+          }),
 
-    toggleCurrentAction: (actionType: ActionType) =>
-      iSet((draft) => {
-        if (actionType === draft.uiState.currentAction.type) {
-          draft.uiState.currentAction = { type: ActionType.none }
-          return
-        }
-
-        switch (actionType) {
-          case ActionType.buildRoad: {
-            draft.uiState.currentAction = {
-              type: actionType,
-              positions: board.getRoadPositions(draft.gameState.tiles),
+        toggleCurrentAction: (actionType: ActionType) =>
+          iSet((draft) => {
+            if (actionType === draft.uiState.currentAction.type) {
+              draft.uiState.currentAction = { type: ActionType.none }
+              return
             }
-            break
-          }
 
-          case ActionType.buildTown: {
-            draft.uiState.currentAction = {
-              type: actionType,
-              positions: board.getTownPositions(draft.gameState.tiles),
+            switch (actionType) {
+              case ActionType.buildRoad: {
+                draft.uiState.currentAction = {
+                  type: actionType,
+                  positions: board.getRoadPositions(draft.gameState.tiles),
+                }
+                break
+              }
+
+              case ActionType.buildTown: {
+                draft.uiState.currentAction = {
+                  type: actionType,
+                  positions: board.getTownPositions(draft.gameState.tiles),
+                }
+                break
+              }
+
+              case ActionType.none: {
+                draft.uiState.currentAction = { type: actionType }
+                break
+              }
+
+              default:
+                const exhaustiveCheck: never = actionType
+                throw new Error(`Unhandled case: ${exhaustiveCheck}`)
             }
-            break
-          }
+          }),
 
-          case ActionType.none: {
-            draft.uiState.currentAction = { type: actionType }
-            break
-          }
-
-          default:
-            const exhaustiveCheck: never = actionType
-            throw new Error(`Unhandled case: ${exhaustiveCheck}`)
-        }
-      }),
-
-    updateGameState: (gameState) =>
-      iSet((draft) => {
-        draft.gameState = gameState
-      }),
-  }
-})
+        updateGameState: (gameState) =>
+          iSet((draft) => {
+            draft.gameState = gameState
+          }),
+      }
+    },
+    { name: 'state' },
+  ),
+)
 
 export default useStore
