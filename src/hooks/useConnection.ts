162090196b2fbd885aa1ecf40477e895d626f737
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-05-15 16:56:55 +0200

    useConnection: Clean up console.logs

--- src/hooks/useConnection.ts
+++ src/hooks/useConnection.ts
@@ -3,6 +3,8 @@ import { useEffect, useState, useRef } from 'react'
 import { useStore } from '../state'
 import usePlayerId from './usePlayerId'
 
+const log = console.log
+
 function useConnection() {
   const [peerId, setPeerId] = useState<string | void>()
   const playerId = usePlayerId()
@@ -20,21 +22,19 @@ function useConnection() {
       return
     }
 
-    console.log(`Connecting to ${playerId}...`)
+    log(`Connecting to ${playerId}...`)
 
     let conn = peerRef.current.connect(playerId)
     conn.on('data', (data) => {
-      console.log(`received: ${data}`)
+      log(`received: ${data}`)
     })
+
     conn.on('open', () => {
-      conn.send('hi!')
       conn.send({ method: 'connect', args: { playerId } })
     })
   }
 
   const sendState = () => {
-    console.log(peerRef.current?.connections)
-    console.log(state.games)
     if (!peerRef.current) {
       return
     }
@@ -54,7 +54,7 @@ function useConnection() {
 
     peerRef.current.on('open', (id) => {
       setPeerId(id)
-      console.log('My peer ID is: ' + id)
+      log('My peer ID is: ' + id)
     })
 
     peerRef.current.on('error', (error) => {
@@ -64,9 +64,10 @@ function useConnection() {
     // Handle incoming data connection
     peerRef.current.on('connection', (conn) => {
       let connectedPlayerId: string
-      console.log('incoming peer connection!')
+      log('incoming peer connection!')
+
       conn.on('data', (data) => {
-        console.log(`received:`, data)
+        log(`received:`, data)
         if (typeof data === 'object') {
           if (data.method === 'connect') {
             connectedPlayerId = data.args.playerId
@@ -75,6 +76,7 @@ function useConnection() {
           updateGameState('a', data)
         }
       })
+
       conn.on('open', () => {
         conn.send('hello!')
       })
