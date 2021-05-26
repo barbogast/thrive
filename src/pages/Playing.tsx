c6818a0204fa6328942575795dbe50d236e94708
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-05-26 17:22:58 +0200

    Playing: Code improvement

--- src/pages/Playing.tsx
+++ src/pages/Playing.tsx
@@ -11,31 +11,30 @@ import Box from '../components/Box'
 
 const Playing: React.FC = function Playing() {
   const gameId = routing.useGameId()
-  const { sequence, currentPlayerColor, currentAction, currentDiceRoll } =
-    useGameStore((state) => ({
-      state: state,
-      sequence: state.games[gameId].sequence,
-      currentAction: state.games[gameId].sequence.scheduledActions[0],
-      currentPlayerColor:
-        state.games[gameId].players[
-          state.games[gameId].sequence.scheduledActions[0].playerId
-        ].color,
-      currentDiceRoll: state.games[gameId].currentDiceRoll,
-    }))
+  const gameStore = useGameStore((state) => ({
+    sequence: state.games[gameId].sequence,
+    currentAction: state.games[gameId].sequence.scheduledActions[0],
+    currentPlayerColor:
+      state.games[gameId].players[
+        state.games[gameId].sequence.scheduledActions[0].playerId
+      ].color,
+    currentDiceRoll: state.games[gameId].currentDiceRoll,
+  }))
 
   return (
     <>
       <button onClick={() => window.history.back()}>Back</button>
       <div>
-        Current player <Box color={currentPlayerColor} />: {currentAction.type}
+        Current player <Box color={gameStore.currentPlayerColor} />:{' '}
+        {gameStore.currentAction.type}
         <br />
       </div>
-      {sequence.phaseType === 'normal' ? (
+      {gameStore.sequence.phaseType === 'normal' ? (
         <>
           <Players />
           <div>
-            {currentAction.type !== game.GameActionType.rollDice
-              ? 'Current dice roll: ' + currentDiceRoll.join(' | ')
+            {gameStore.currentAction.type !== game.GameActionType.rollDice
+              ? 'Current dice roll: ' + gameStore.currentDiceRoll.join(' | ')
               : ''}
           </div>
           <Controls />
