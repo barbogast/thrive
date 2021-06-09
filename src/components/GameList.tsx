37183692c64f85984286c91ccdb27c5b022a1ddf
Benjamin Arbogast <benjamin.arbogast@gmail.com>
2021-06-09 09:36:34 +0200

    Extract GameList into separate component

---
+++ src/components/GameList.tsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from 'wouter'
+
+import { useLocalStore } from '../state/localState'
+import ConnectionStatus from '../components/ConnectionStatus'
+import { useGameStore } from '../state/gameState'
+import { GameState } from '../game'
+
+function formatPlayers(game: GameState) {
+  const allPlayers = Object.keys(game.players)
+  const allExceptTheLast = allPlayers.slice(0, -1)
+  const theLast = allPlayers[allPlayers.length - 1]
+  return (
+    allExceptTheLast.map((playerId) => game.players[playerId].name).join(', ') +
+    ' and ' +
+    game.players[theLast].name
+  )
+}
+const GameList: React.FC = function GameList() {
+  const localStore = useLocalStore((state) => ({
+    myId: state.myId,
+  }))
+  const gameStore = useGameStore((state) => ({
+    games: state.games,
+  }))
+
+  return (
+    <div>
+      Existing games
+      <ul>
+        {Object.entries(gameStore.games).map(([gameId, game]) => (
+          <li key={gameId}>
+            <Link href={`/play/${gameId}`}>
+              <a className="link">Continue game with {formatPlayers(game)}</a>
+            </Link>
+            {Object.values(game.players).map((player) => (
+              <span key={player.id}>
+                {player.name}
+                {player.peerId !== localStore.myId ? (
+                  <ConnectionStatus id={player.peerId} />
+                ) : (
+                  <></>
+                )}
+              </span>
+            ))}
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default GameList
--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,31 +1,17 @@
 import React from 'react'
-import { Link } from 'wouter'
 import { nanoid } from 'nanoid'
 import { useLocation } from 'wouter'
 
 import { useLocalStore } from '../state/localState'
 import PlayerName from '../components/PlayerName'
 import FriendsList from '../components/FriendsList'
-import ConnectionStatus from '../components/ConnectionStatus'
 import BoardSettingsForm from '../components/BoardSettingsForm'
+import GameList from '../components/GameList'
 import {
   addLocalPlayer,
   createGame,
   removeSelectedPlayers,
 } from '../state/setters'
-import { useGameStore } from '../state/gameState'
-import { GameState } from '../game'
-
-function formatPlayers(game: GameState) {
-  const allPlayers = Object.keys(game.players)
-  const allExceptTheLast = allPlayers.slice(0, -1)
-  const theLast = allPlayers[allPlayers.length - 1]
-  return (
-    allExceptTheLast.map((playerId) => game.players[playerId].name).join(', ') +
-    ' and ' +
-    game.players[theLast].name
-  )
-}
 
 const MainMenu: React.FC = function MainMenu() {
   const [, setLocation] = useLocation()
@@ -34,9 +20,6 @@ const MainMenu: React.FC = function MainMenu() {
     set: state.set,
     myId: state.myId,
     friends: state.friends,
-  }))
-  const gameStore = useGameStore((state) => ({
-    games: state.games,
   }))
 
   const create = () => {
@@ -60,28 +43,7 @@ const MainMenu: React.FC = function MainMenu() {
   return (
     <>
       <PlayerName label="Your name" />
-      <div>
-        Existing games
-        <ul>
-          {Object.entries(gameStore.games).map(([gameId, game]) => (
-            <li key={gameId}>
-              <Link href={`/play/${gameId}`}>
-                <a className="link">Continue game with {formatPlayers(game)}</a>
-              </Link>
-              {Object.values(game.players).map((player) => (
-                <span key={player.id}>
-                  {player.name}
-                  {player.peerId !== localStore.myId ? (
-                    <ConnectionStatus id={player.peerId} />
-                  ) : (
-                    <></>
-                  )}
-                </span>
-              ))}
-            </li>
-          ))}
-        </ul>
-      </div>
+      <GameList />
       <div>
         Contacts
         <div>
